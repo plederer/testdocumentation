35cbcc694cec147527b1d756163af50f6b6f7c2c
Philip <philip@u24.at>
2023-08-23 13:03:21 +0900

    docu files

---
+++ source/docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+# sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
+sys.path.append(os.path.abspath('../../source'))
+print(sys.path)
+
+from myclass import myFruit
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'mydoc'
+copyright = '2023, PL'
+author = 'PL'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+
+import sphinx_rtd_theme
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_static_path = ['_static']
---
+++ source/docs/source/index.rst
@@ -0,0 +1,21 @@
+.. mydoc documentation master file, created by
+   sphinx-quickstart on Mon Aug 21 14:40:33 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Hello my world
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   usage
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
